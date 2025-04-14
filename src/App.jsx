5f05bd20cb3a9673cf84855f5c828ef76c7da7b9
ruhani-png <ruhanisehgal03@gmail.com>
2025-04-14 13:09:05 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-pink-100 text-purple-800 text-3xl font-bold">
+      Hello, Spotify Clone! 💖🎶
+    </div>
+  )
+}
+
+export default App
